598a08079ed120cc3bd4b6a6deb0812331c39b20
Joni Mertoniemi <joni.mertoniemi@sysart.fi>
2017-12-12 14:41:12 +0200

    fix teams

--- src/components/Teams.vue
+++ src/components/Teams.vue
@@ -1,36 +1,43 @@
 <template>
-  <v-container fluid grid-list-md>
-    <v-layout row wrap>
-      <v-flex xs12 sm6 v-for="team in teams" :key="team['.key']">
-        <v-card>
-          <v-container>
-            <form @submit.prevent="update(team)">
-              <v-text-field label="Name" v-model="team.name" />
-              <v-text-field label="Emoji" v-model="team.emoji" />
-              <v-btn @click="update(team)">Update</v-btn>
-              <v-btn color="error" @click="remove(team)">Remove</v-btn>
-              <h2>Members ({{team.members && Object.keys(team.members).length}}):</h2>
-              <div v-for="member in team.members" :key="member['.key']">
-                {{member.name}}
-              </div>
-            </form>
-          </v-container>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card>
-          <v-container>
-            <form @submit.prevent="create">
-              <v-text-field label="Name" v-model="name" />
-              <v-btn @click="create">Create</v-btn>
-            </form>
-          </v-container>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <md-card v-for="team in teams" :key="team['.key']">
+      <md-card-content>
+        <form @submit.prevent="update(team)">
+          <md-field>
+            <label>Name</label>
+            <md-input
+              v-model="team.name"
+            ></md-input>
+          </md-field>
+          <md-field>
+            <label>Emoji</label>
+            <md-input
+              v-model="team.emoji"
+            ></md-input>
+          </md-field>
+          <md-button class="md-raised md-primary" @click="update(team)">Update</md-button>
+          <md-button class="md-raised md-accent" @click="remove(team)">Remove</md-button>
+          <h2>Members ({{team.members && Object.keys(team.members).length}}):</h2>
+          <div v-for="member in team.members" :key="member['.key']">
+            {{member.name}}
+          </div>
+        </form>
+      </md-card-content>
+    </md-card>
+    <md-card>
+      <md-card-content>
+        <form @submit.prevent="create">
+          <md-field>
+            <label>Name</label>
+            <md-input
+              v-model="name"
+            ></md-input>
+          </md-field>
+          <md-button class="md-raised md-primary" @click="create">Create</md-button>
+        </form>
+      </md-card-content>
+    </md-card>
+  </div>
 </template>
 
 <script>
@@ -70,3 +77,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
